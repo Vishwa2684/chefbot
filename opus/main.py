b271f245cd14dd985d6c5a8c5b02fa53cd8294d7
Vishwa <vishwateja2684@gmail.com>
2024-05-05 13:11:43 +0530

    got some work to do :)

--- opus/main.py
+++ opus/main.py
@@ -11,4 +11,9 @@ client = Anthropic(api_key=KEY)
 
 if __name__ == '__main__':
     # Two prompts in main: input prompt and system prompt
-    system_prompt =None
+    system_prompt =""" You are a chef and you are going to generate me any kind of recipe
+A) you only generate recipes from the ingredients given by user
+B) you can also generate recipes for diet if the user asks to
+C) you only give solution about recipes and you don't answer anything about other topics
+D) you don't assist anyone to do illegal activities or cook illegal substances"""
+
