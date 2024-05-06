df696c168727406f2488ea8e2af3d290f00be699
Vishwa <vishwateja2684@gmail.com>
2024-05-06 11:00:48 +0530

    work on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Chat from './Pages/Chat/Chat'
+import Sidebar from './Components/Sidebar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
-    <Chat/>
+    <div className='sidebar'>
+      <Sidebar/>
+    </div>
+    <div className="chat">
+      <Chat/>
+    </div>
       
     </>
   )
--- frontend/src/Pages/Chat/Chat.jsx
+++ frontend/src/Pages/Chat/Chat.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react'
 import './Chat.css'
 import RecipeCard from '../../Components/RecipeCard'
+import axios from 'axios'
+
+import  Button  from 'react-bootstrap/Button'
+
+
 export default function Chat() {
     const [prompt,setPrompt] = useState(null)
     const [results,setResults] = useState([])
 
     
     const action = async ()=>{
-      const response = await fetch(`https://api.edamam.com/search?q=${prompt}&app_id=7aa516a5&app_key=dc836a223fb788b11ae390504d9e97ce&from=0&to=10`);
-      if(response.ok){
-        const data= await response.json()
+      const response = await axios.get(`https://api.edamam.com/search?q=${prompt}&app_id=7aa516a5&app_key=dc836a223fb788b11ae390504d9e97ce&from=0&to=10`);
+      if(response.status === 200){
+        const data= await response.data
         console.log(`response ok`)
         setResults(data.hits)
       }
@@ -17,16 +22,25 @@ export default function Chat() {
     
     return (
       <div className="container">
-          <h1>Sanji</h1>
+          
+          {prompt && (
+            <div className="chat-prompt">
+              You: {prompt}
+            </div>
+          )}
 
-            {results.map(recipe => (
+          {results.map(recipe => (
               <RecipeCard image={recipe.recipe.image} label ={recipe.recipe.label} url={recipe.recipe.url} ingredientLines={recipe.recipe.ingredientLines}/> 
             ))}
-              <input type="text" id="search" placeholder="Enter ingredients or your recipe....." onChange={(e)=>{
-                setPrompt(e.target.value)
-              }}/>
-              <button type="button" id="submit" className="btn" onClick={action}>Search</button>
-            
+
+            <div className='search'>
+                <input type="text" id="search" placeholder="Enter ingredients or your recipe....." onChange={(e)=>{
+                    setPrompt(e.target.value)
+                  }}/>
+                
+                  <Button onClick={action}>search</Button>
+            </div>
+              
       </div>
 
   )
