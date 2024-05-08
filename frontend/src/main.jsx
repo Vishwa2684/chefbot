ce3f1905eefee3639b62c3462a89c4b71b3ffb5b
Vishwa <vishwateja2684@gmail.com>
2024-05-08 09:47:21 +0530

    home

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter ,Route,Routes} from'react-router-dom'
+import Home from './Pages/Home.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<App />}/>
+      <Route path='/' element = {<Home/>}></Route>
+      <Route path='/c' element={<App />}/>
     </Routes>
     
   </BrowserRouter>
