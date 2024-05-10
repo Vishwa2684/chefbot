3910d963feb7601fa4a8c9bee2d72ad3c7f7b501
Vishwa <vishwateja2684@gmail.com>
2024-05-10 15:30:41 +0530

    should set permissions for ordering

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
